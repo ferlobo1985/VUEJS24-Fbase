89808398ed09196319059f1db8e6fcc64b31b292
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-10 18:07:04 -0300

    02-Install and config FBase
    02-Install and config FBase

---
+++ src/firebase/configs.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCreyHRcewBaQLwoO9rqwGZEkpF9d2l0Zk",
+    authDomain: "fbasep-8e300.firebaseapp.com",
+    projectId: "fbasep-8e300",
+    storageBucket: "fbasep-8e300.appspot.com",
+    messagingSenderId: "1010404054519",
+    appId: "1:1010404054519:web:dba3b5b6e23d9523e68af2"
+};
+
+initializeApp(firebaseConfig);
+
+//// DB
+const DB = getFirestore();
+export { DB }
